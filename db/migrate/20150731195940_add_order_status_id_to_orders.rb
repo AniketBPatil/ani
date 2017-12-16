7f469256e238cf6009835181f3b60ec30db0af8a
Aniket <aniket@Aniket.(none)>
2017-12-16 08:50:41 +0530

    Deleting unused migration file

--- db/migrate/20150731195940_add_order_status_id_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderStatusIdToOrders < ActiveRecord::Migration
-  def change
-    add_reference :orders, :order_status, index: true, foreign_key: true
-  end
-end
--- db/migrate/20150731200349_remove_order_status_id_from_orders.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOrderStatusIdFromOrders < ActiveRecord::Migration
-  def change
-    remove_column :orders, :orderstatus_id, :integer
-  end
-end
